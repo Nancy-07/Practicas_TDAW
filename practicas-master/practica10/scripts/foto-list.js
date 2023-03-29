44d1e85b58fb18e18c92bfb243837f163cbc4306
Nan08 <nan.galicia03@gmail.com>
2023-03-29 17:01:14 -0600

    subimos las practicas

---
+++ practicas-master/practica10/scripts/foto-list.js
@@ -0,0 +1,11 @@
+const PHOTO_LIST =[
+    'image/conejo.jpg',
+    'image/gato.jpg',
+    'image/lobo.jpg',
+    'image/mono.jpg',
+    'image/panda.jpg',
+    'image/perro.jpeg',
+    'image/suricata.jpg',
+    'image/tigre.jpg',
+    'image/zorro.jpg'
+];
